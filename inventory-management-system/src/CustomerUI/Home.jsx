95874161229b9cea065d21b6d8e681c4b17efd25
Jeremy Olea <jeremy.olea18@gmail.com>
2019-11-20 10:31:14 -0700

    added login stuff

--- inventory-management-system/src/CustomerUI/Home.jsx
+++ inventory-management-system/src/CustomerUI/Home.jsx
@@ -7,13 +7,39 @@ import 'react-tabs/style/react-tabs.css';
 class Home extends Component {
   constructor(props){
     super(props);
+    this.state = {
+        items: {},
+        purchases: {},
+        cart: {},
+        email: "",
+        password: "",
+    };
+
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+  }
+
+  handleEmailChange(event) {
+    this.setState({email: event.target.value});
+  }
+
+  handlePasswordChange(event){
+    this.setState({password: event.target.value});
   }
 
   render(){
     return (
       <div className="Home">
         <div>
-        Shop Name<br></br><br></br>
+          <form>
+            Shop Name
+            <span className="login">
+              <input type="text" placeholder="Email" value={this.state.email} onChange={this.handleEmailChange}></input>
+              <input type="password" placeholder="Password" value={this.state.password} onChange={this.handlePasswordChange}></input>
+            </span>
+          </form>
+        <div>
+        </div>
         {/* <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" /> */}
           <Tabs defaultIndex={1}>
             <TabList>
