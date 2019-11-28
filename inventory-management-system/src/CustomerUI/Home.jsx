032ceb1e869bbcce53683a238f8109254f3dcb82
Anton Santos <43733827+antondotio@users.noreply.github.com>
2019-11-27 18:45:36 -0700

    Update Home.jsx

--- inventory-management-system/src/CustomerUI/Home.jsx
+++ inventory-management-system/src/CustomerUI/Home.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './Home.css';
-import {Tab, Tabs, TabList, TabPanel} from "react-tabs";
+import {Tab, Tabs, TabList, TabPanel} from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 // import {Redirect} from 'react-router-dom';
 
