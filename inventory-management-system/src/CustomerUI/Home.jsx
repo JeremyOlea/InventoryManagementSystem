6c3c8d590f8c192d1efa39e6b4d9c0b494693dc4
FreeKodak <daniel_orduz19@outlook.com>
2019-12-01 12:54:03 -0700

    Few changes

--- inventory-management-system/src/CustomerUI/Home.jsx
+++ inventory-management-system/src/CustomerUI/Home.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import './Home.css';
 import {Tab, Tabs, TabList, TabPanel} from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
