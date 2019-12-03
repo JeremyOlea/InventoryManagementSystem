aa170fb4c66752e0d0e80781e670eb27a76b51b7
Jeremy Olea <jeremy.olea18@gmail.com>
2019-12-03 10:10:46 -0700

    added signup to route

--- inventory-management-system/src/index.js
+++ inventory-management-system/src/index.js
@@ -5,6 +5,7 @@ import './index.css';
 import App from './App';
 import Item from './CustomerUI/Item';
 import * as serviceWorker from './serviceWorker';
+import Signup from './Signup'
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
@@ -17,6 +18,7 @@ const routing = (
         <div>
             <Route exact path="/" component={App} />
             <Route path="/Item/:itemId" component={Item}/>
+            <Route path="/signup" component={Signup}/>
         </div>
     </Router>
 )
